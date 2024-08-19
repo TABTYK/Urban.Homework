f46ce59528020634bfb55444fa92b7e5f5bacaeb
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-08-19 08:06:15 +0300

    Update lesson dictionaries and sets.py

--- lesson dictionaries and sets.py
+++ lesson dictionaries and sets.py
@@ -7,7 +7,7 @@ my_dict.update({'Tester': 000000,
                 'User2': 606060})
 print(my_dict)
 del my_dict['User2']
-print(my_dict)
+print(my_dict.get('User2'))
 
 my_set = {3,3,3,2,2,1}
 print(my_set)
