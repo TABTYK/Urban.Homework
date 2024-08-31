7e8e598117403ad3f1b799253d625c926791f356
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-08-31 10:02:59 +0300

    Update 02 Additional assigment.py

--- module 2/02 Additional assigment.py
+++ module 2/02 Additional assigment.py
@@ -4,10 +4,10 @@ def lovushka(n):
     for i in range(1,n):
         for j in range(1,n):
             divider = i + j
-            if i == j:
+            if i == j: 
                 break
             if n % divider ==0:
-                if result.count(i) and result.count(j):
+                if result.count(i) and result.count(j): # проверяет есть ли j и i в списке result
                     break
                 result.append(j)
                 result.append(i)
