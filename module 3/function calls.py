f04ad5bcf058dc6289ab6ee81efa73f906c2a06a
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-09-02 11:15:28 +0300

    Update function calls.py

--- module 3/function calls.py
+++ module 3/function calls.py
@@ -5,7 +5,7 @@ def send_email(message, recipient, sender = "university.help@gmail.com"):
         elif recipient == sender:
             print("Нельзя отправить письмо самому себе!")
         else:
-            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
     else:
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
