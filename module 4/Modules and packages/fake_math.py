311fba02d9bfb874daf50843784d2d4c7e750cdd
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-09-07 12:04:11 +0300

    Add files via upload

---
+++ module 4/Modules and packages/fake_math.py
@@ -0,0 +1,8 @@
+def divide(first,second):
+    if first == 0 or second == 0:
+        return 'Mistake'
+    else:
+        return first / second
+
+print(divide(12,2))
+print(divide(12,0))
---
+++ module 4/Modules and packages/module4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as div_fake
+from true_math import divide as div_true
+
+print(div_fake(12,2))
+print(div_fake(12,0))
+print(div_true(52,4))
+print(div_true(52,0))
