8fbe1e68932cf351a3daaa176e0aadc5d51416dd
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-10-20 13:20:14 +0300

    Add files via upload

---
+++ module 8/module8_3.py
@@ -0,0 +1,52 @@
+from errno import ECHILD
+
+
+class Car():
+    def __is_valid_vin(self,vin_number):
+        if isinstance(vin_number,int):
+            if vin_number in range(1000000,9999999):
+                return True
+            else:
+                try:
+                    raise IncorrectVinNumber('Неверный диапазон для vin номера')
+                except IncorrectVinNumber as exc:
+                    print(exc.message)
+        else:
+            try:
+                raise IncorrectVinNumber('Некорректный тип vin номер')
+            except IncorrectVinNumber as exc:
+                print(exc.message)
+
+    def __is_valid_numbers(self,numbers):
+        if isinstance(numbers, str):
+            if len(numbers) == 6:
+                return True
+            else:
+                try:
+                    raise IncorrectCarNumbers('Неверная длина номера')
+                except IncorrectCarNumbers as exc:
+                    print(exc.message)
+        else:
+            try:
+                raise IncorrectCarNumbers('Некорректный тип numbers')
+            except IncorrectVinNumber as exc:
+                print(exc.message)
+
+    def __init__(self,model = str(),__vin = int(), __numbers = str()):
+        self.model = model
+        self.__vin = __vin
+        self.__numbers = __numbers
+        if self.__is_valid_vin(__vin) and self.__is_valid_numbers(__numbers):
+            print(f'{self.model} успешно создан')
+
+class IncorrectVinNumber(Exception):
+    def __init__(self,message):
+        self.message = message
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+first = Car('Model1', 1000000, 'f123dj')
+second = Car('Model2', 300, 'т001тр')
+third = Car('Model3', 2020202, 'нет номера')
