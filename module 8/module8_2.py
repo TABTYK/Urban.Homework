e643d2f1ffc3c2f5c5e5a4f62a2a4d202a586909
TABTYK <105810672+TABTYK@users.noreply.github.com>
2024-10-22 18:09:52 +0300

    Update module8_2.py

--- module 8/module8_2.py
+++ module 8/module8_2.py
@@ -7,7 +7,7 @@ def personal_sum(numbers):
             result += i
         except TypeError:
             incorrect_data +=1
-            print(i)
+            print('Некоректный тип данных для посчета суммы - ',i)
     return {'result':result,'incorrect_data':incorrect_data}
 
 def calculate_average(numbers):
@@ -22,7 +22,7 @@ def calculate_average(numbers):
         try:
             return Woo.get('result') / (len(numbers) - delitel)
         except ZeroDivisionError:
-            print('На ноль делить нельзя')
+            return 0
     except TypeError:
         print('Работаем только с коллекциями')
         return None
@@ -32,4 +32,3 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
-
